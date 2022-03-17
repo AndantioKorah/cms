9cde1032a304932365d211674078b5441d78a861
tiokors <andantiokorah@gmail.com>
2022-03-17 15:32:59 +0800

    perbaikan

--- application/config/constants.php
+++ application/config/constants.php
@@ -98,9 +98,9 @@ define('KODE_TRANSAKSI_PEMBAYARAN', '02');
 define('KODE_TRANSAKSI_UANG_MUKA', '03');
 
 define('VERSION', 'Version 1.0');
-define('TITLES', 'APANG | BKPSDM Kota Manado');
-define('TITLE_SECOND', 'APLIKASI PENILAIAN KINERJA PEGAWAI');
-define('TITLE_THIRD', 'APANG');
+define('TITLES', 'BIDIK - Asn Juara | BKPSDM Kota Manado');
+define('TITLE_SECOND', 'BIDIK - Asn Juara');
+define('TITLE_THIRD', 'BIDIK');
 define('PROGRAMMER_PHONE', '082115407812');
 define('COPYRIGHT', 'Copyright &copy; '.date('Y').' <strong>NiKita</strong>');
 // define('COPYRIGHT', 'Copyright &copy; '.date('Y').' <strong>PATRA LAB</strong>');
