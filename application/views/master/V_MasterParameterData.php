cd2fe79f1e38868bab850357481f28b579fdcbbc
bukantio <andantiokorah@gmail.com>
2022-07-21 00:34:49 +0800

    perbaikan

--- application/views/master/V_MasterParameterData.php
+++ application/views/master/V_MasterParameterData.php
@@ -1,26 +1,28 @@
 <?php if($result){ ?>
-    <table class="table" id="table_master_parameter">
-        <thead>
-            <th class="text-center">No</th>
-            <th class="text-left">Parameter Name</th>
-            <th class="text-left">Parameter Value</th>
-            <th class="text-center">Pilihan</th>
-        </thead>
-        <tbody>
-            <?php $no = 1; foreach($result as $rs){ ?>
-                <tr>
-                    <td style="width: 5%" class="text-center"><?=$no++;?></td>
-                    <td style="width: 20%" class="text-left"><?=$rs['parameter_name']?></td>
-                    <td style="width: 55%" class="text-left"><?=$rs['parameter_value']?></td>
-                    <td style="width: 20%" class="text-center">
-                        <button data-toggle="modal" href="#modal_data_parameter" onclick="editParameter('<?=$rs['id']?>')" class="btn btn-sm btn-warning text-white"><i class="fa fa-edit"></i> Edit</button>
-                        <button onclick="deleteParameter('<?=$rs['id']?>')" id="btn_delete_<?=$rs['id']?>" class="btn btn-sm btn-danger"><i class="fa fa-trash"></i> Hapus</button>
-                        <button disabled style="display: none;" id="btn_loading_<?=$rs['id']?>" class="btn btn-sm btn-danger"><i class="fa fa-spin fa-spinner"></i> Menghapus....</button>
-                    </td>
-                </tr>
-            <?php } ?>
-        </tbody>
-    </table>
+    <div class="col-12 table-responsive">
+        <table class="table" id="table_master_parameter">
+            <thead>
+                <th class="text-center">No</th>
+                <th class="text-left">Parameter Name</th>
+                <th class="text-left">Parameter Value</th>
+                <th class="text-center">Pilihan</th>
+            </thead>
+            <tbody>
+                <?php $no = 1; foreach($result as $rs){ ?>
+                    <tr>
+                        <td style="width: 5%" class="text-center"><?=$no++;?></td>
+                        <td style="width: 20%" class="text-left"><?=$rs['parameter_name']?></td>
+                        <td style="width: 55%" class="text-left"><?=$rs['parameter_value']?></td>
+                        <td style="width: 20%" class="text-center">
+                            <button data-toggle="modal" href="#modal_data_parameter" onclick="editParameter('<?=$rs['id']?>')" class="btn btn-sm btn-warning text-white"><i class="fa fa-edit"></i> Edit</button>
+                            <button onclick="deleteParameter('<?=$rs['id']?>')" id="btn_delete_<?=$rs['id']?>" class="btn btn-sm btn-danger"><i class="fa fa-trash"></i> Hapus</button>
+                            <button disabled style="display: none;" id="btn_loading_<?=$rs['id']?>" class="btn btn-sm btn-danger"><i class="fa fa-spin fa-spinner"></i> Menghapus....</button>
+                        </td>
+                    </tr>
+                <?php } ?>
+            </tbody>
+        </table>
+    </div>
     
     <script>
         $(function(){
