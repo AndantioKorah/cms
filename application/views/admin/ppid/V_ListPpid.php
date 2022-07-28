2426587b2d11483a9459a17cb22395b1a8c17796
youri <torehyouri@gmail.com>
2022-07-28 23:57:44 +0800

    pull

---
+++ application/views/admin/ppid/V_ListPpid.php
@@ -0,0 +1,68 @@
+<?php if($list_ppid){ ?>
+    <div class="table-responsive">
+    <table id="example" class="table table-striped table-bordered" style="width:100%">
+        <thead>
+            <tr>
+                <th>No</th>
+                <th>Judul </th>
+                <th>File</th>
+                <th></th>
+            </tr>
+        </thead>
+        <tbody>
+        <?php $no=1; foreach($list_ppid as $lb){ ?>
+                   <tr>
+                    <td><?=$no++;?></td>
+                    <td><?=$lb['judul'];?></td>
+                    <td> <a style='width:800;height:300px;' href="<?=base_url('assets/admin/ppid/'.$lb['file'].'')?>" target="_blank"><?=base_url('assets/admin/galeri/'.$lb['file'].'')?></a> </td>
+                  <td> <button onclick="deletePpid('<?=$lb['id']?>')" class="btn btn-sm btn-danger" data-toggle="tooltip" data-placement="top" title="Hapus"><i class="fa fa-trash" ></i></button></td>
+                   </tr>
+                <?php } ?>
+        </tfoot>
+    </table>
+    </div>
+        <!-- Modal Ubah -->
+<div aria-hidden="true" aria-labelledby="myModalLabel" role="dialog" tabindex="-1" id="edit-data" class="modal fade">
+    <div class="modal-dialog modal-xl">
+        <div class="modal-content">
+            <div class="modal-header">
+                <button aria-hidden="true" data-dismiss="modal" class="close" type="button">×</button>
+                <h4 class="modal-title"></h4>
+            </div>
+            <div class="modal-body" id="modal_detail_berita">
+            
+                     
+                     
+                 </div>
+                
+               
+            </div>
+        </div>
+    </div>
+</div>
+<!-- END Modal Ubah -->
+    <?php } else { ?>
+    <div class="row">
+        <div class="col-12">
+            <h5 style="text-center">BELUM ADA DATA <i class="fa fa-exclamation"></i></h5>
+        </div>
+    </div>
+<?php } ?>
+<script>
+        function deletePpid(id){
+           
+           if(confirm('Apakah Anda yakin ingin menghapus data?')){
+               $.ajax({
+                   url: '<?=base_url("admin/C_admin/deletePpid/")?>'+id,
+                   method: 'post',
+                   data: null,
+                   success: function(){
+                       successtoast('Data sudah terhapus')
+                       loadListPpid()
+                   }, error: function(e){
+                       errortoast('Terjadi Kesalahan')
+                   }
+               })
+           }
+       }
+</script>
